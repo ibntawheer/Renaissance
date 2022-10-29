2f33672a39ce18acc8d8f436bd91cc828256475e
ibntawheer <bilalsadik3@gmail.com>
2022-10-28 22:08:18 -0700

    Initial Commit

---
+++ Renaissance/RenaissanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenaissanceApp.swift
+//  Renaissance
+//
+//  Created by Bilal Khan on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RenaissanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
